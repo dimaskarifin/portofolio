dc141932918c0ba3db0454af10d8c8b4b1aa5861
dimaskarifin <dimaskarifin@gmail.com>
2023-03-11 02:41:06 +0700

    [update] : Header

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,14 +1,78 @@
-import React from "react";
+import { SocialIcon } from "react-social-icons";
+import { motion } from "framer-motion";
 
 type Props = {};
 
 function Header({}: Props) {
   return (
-    <header>
-      <div>
+    <header className="sticky top-0 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center p-5">
+      <motion.div
+        initial={{
+          x: -500,
+          opacity: 0,
+          scale: 0.5,
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{
+          duration: 1.5,
+        }}
+        className="flex flex-row items-center"
+      >
         {/* Social Media Icons */}
-        <h1></h1>
-      </div>
+        <SocialIcon
+          url="https://www.instagram.com/dimaskarifin"
+          target="_blank"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+        <SocialIcon
+          url="https://www.facebook.com/dimaskarifin"
+          target="_blank"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+        <SocialIcon
+          url="https://t.me/dimaskarifin"
+          network="telegram"
+          target="_blank"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+      </motion.div>
+
+      {/* contact me */}
+      <motion.div
+        initial={{
+          x: 500,
+          opacity: 0,
+          scale: 0.5,
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{
+          duration: 1.5,
+        }}
+        className="flex flex-row items-center text-gray-300 cursor-pointer"
+      >
+        <SocialIcon
+          className="cursor-point"
+          url=""
+          network="email"
+          target="_blank"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+        <p className="uppercase hidden md:inline-flex text-sm text-gray-400">
+          get in touch
+        </p>
+      </motion.div>
     </header>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Header from "@/components/Header";
+import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
@@ -15,6 +16,9 @@ export default function Home() {
       <Header />
 
       {/* Hero Section */}
+      <section id="hero">
+        <Hero />
+      </section>
 
       {/* About Section */}
 
