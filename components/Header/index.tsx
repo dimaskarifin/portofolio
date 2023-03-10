70b397e683334e0bc75677dd091f90a259b5cbd3
dimaskarifin <dimaskarifin@gmail.com>
2023-03-11 01:52:25 +0700

    First Commit

---
+++ components/Header/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+type Props = {};
+
+function Header({}: Props) {
+  return (
+    <header>
+      <div>
+        {/* Social Media Icons */}
+        <h1></h1>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
